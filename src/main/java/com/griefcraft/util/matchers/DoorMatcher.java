dc1316c7b6d53b195501c45dda31396156c675ef
pop4959 <pop4959@gmail.com>
2020-05-05 15:28:10 -0700

    Revert "Don't match door if neither door block is protected"
    This reverts commit 4acf69e627e0118ad68f3e1727cabd9d6e8cc2cd.

--- src/main/java/com/griefcraft/util/matchers/DoorMatcher.java
+++ src/main/java/com/griefcraft/util/matchers/DoorMatcher.java
@@ -126,10 +126,6 @@ public class DoorMatcher implements ProtectionFinder.Matcher {
         // Match the bottom half of the door
         else if (PROTECTABLES_DOORS.contains(aboveBaseBlock.getType())) {
             finder.addBlock(aboveBaseBlock);
-            // At this point, all door blocks are added. If there is no protection found, we can and should stop.
-            if (finder.loadProtection(true) == null) {
-                return false;
-            }
             finder.addBlock(block.getRelative(BlockFace.DOWN));
             findPressurePlate(finder, block);
             return true;
@@ -138,11 +134,8 @@ public class DoorMatcher implements ProtectionFinder.Matcher {
         // Match the top half of the door
         else if (PROTECTABLES_DOORS.contains(baseBlockState.getType())) {
             Block bottomHalf = block.getRelative(BlockFace.DOWN);
+
             finder.addBlock(bottomHalf);
-            // At this point, all door blocks are added. If there is no protection found, we can and should stop.
-            if (finder.loadProtection(true) == null) {
-                return false;
-            }
             finder.addBlock(bottomHalf.getRelative(BlockFace.DOWN));
             findPressurePlate(finder, bottomHalf);
             return true;
